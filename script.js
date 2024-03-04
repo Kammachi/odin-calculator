eb54286fc3c158174e626358fdb9173fe1b3ddea
Kammachi <126817246+Kammachi@users.noreply.github.com>
2024-03-04 21:42:19 +0100

    Add a decimal point and keyboard support

--- script.js
+++ script.js
@@ -16,10 +16,9 @@ function divide(n1, n2) {
 }
 
 
-
 function operate(n1, n2, operator) {
-    n1 = parseInt(n1);
-    n2 = parseInt(n2);
+    n1 = parseFloat(n1);
+    n2 = parseFloat(n2);
     switch (operator) {
         case "+":
             return add(n1, n2);
@@ -27,29 +26,37 @@ function operate(n1, n2, operator) {
             return subtract(n1, n2);
         case "x":
             return multiply(n1, n2);
-        case "÷":
+        case "/":
             return divide(n1, n2);
         default:
-            return "";
+            return "ERROR";
     }
 }
 
-
-const display = document.querySelector('#display');
 
 let number1;
 let operator;
 let operatorLast = false;
 
+const calcDisplay = document.querySelector('#display');
 
 const numbers = document.querySelectorAll('.digit');
 
 numbers.forEach((number) => number.addEventListener('click', () => {
-    if (operator && operatorLast == true || display.textContent == "NO") {
-        display.textContent = "";
+    if (operator && operatorLast == true || calcDisplay.textContent == "NO") {
+        calcDisplay.textContent = "";
     }
 
-    display.textContent += number.value;
+    if (number.value == "." && calcDisplay.textContent.includes(".")) return;
+
+    if (calcDisplay.textContent.length > 15) return;
+
+    if (calcDisplay.textContent === "0") {
+        calcDisplay.textContent = number.value;
+    } else {
+        calcDisplay.textContent += number.value;
+    }
+
     operatorLast = false;
 }));
 
@@ -60,39 +67,36 @@ operators.forEach((operatorKey) => operatorKey.addEventListener('click', () => {
     
     if (!operatorLast) {
         if (!operator) {
-            number1 = display.textContent;
-            display.textContent = "";
+            number1 = calcDisplay.textContent;
+            calcDisplay.textContent = "";
         } else {
-            number1 = operate(number1, display.textContent, operator)
+            number1 = operate(number1, calcDisplay.textContent, operator)
 
-            if (number1.toString().length > 10) {
-                number1 = Math.round(number1 * 100000000) / 100000000;
+            if (number1.toString().length > 15) {
+                number1 = Math.round(number1 * 10**15) / 10**15;
             }
 
-            display.textContent = number1;
+            calcDisplay.textContent = number1;
         }
     }
 
     operator = operatorKey.value;
     operatorLast = true;
-
 }));
 
 
-const evaluateButton = document.querySelector('#evaluate');
+const evaluateButton = document.querySelector('.evaluate');
 
 evaluateButton.addEventListener('click', () => {
-    if (number1 && display.textContent) {
-        number1 = operate(number1, display.textContent, operator)
+    if (number1 && calcDisplay.textContent) {
+        number1 = operate(number1, calcDisplay.textContent, operator)
 
-        if (number1.toString().length > 10) {
-            number1 = Math.round(number1 * 100000000) / 100000000;
+        if (number1.toString().length > 15) {
+            number1 = Math.round(number1 * 10**15) / 10**15;
         }
 
-        display.textContent = number1;
+        calcDisplay.textContent = number1;
         operator = null;
-    } else {
-        display.textContent = number1;
     }
 });
 
@@ -103,5 +107,35 @@ clear.addEventListener('click', () => {
     number1 = null;
     operator = null;
     operatorLast = false;
-    display.textContent = "";
+    calcDisplay.textContent = "";
+});
+
+
+const deleteButton = document.querySelector('.delete');
+
+deleteButton.addEventListener('click', () => {
+
+    if (calcDisplay.textContent.length === 1) {
+        calcDisplay.textContent = 0;
+    } else if (calcDisplay.textContent.length > 1) {
+        calcDisplaySplit = calcDisplay.textContent.split("");
+        calcDisplaySplit.pop();
+        calcDisplay.textContent = calcDisplaySplit.join("");
+    }
+});
+
+
+document.addEventListener('keydown', (event) => {
+
+    console.log(event)
+
+    const selected = document.getElementById(event.key);
+    
+    if (!selected) {
+        return;
+    }
+
+    let clickEvent = new Event('click');
+
+    selected.dispatchEvent(clickEvent);
 });
